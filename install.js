f02f9eec6d14ace46ac2b12ef2b102b8f1e55d77
Dhirendra Biswal <dhirendrabiswal9@live.com>
2023-01-26 00:29:46 +0100

    fixed a minor error in install.js

--- install.js
+++ install.js
@@ -4,5 +4,5 @@ const files = ['errors.json','failed.json','id_failed.json','list.json','log.jso
 fs.writeFileSync('./data/system.json',JSON.stringify(start),error=> console.log(error));
 
 files.forEach(x =>{
-    fs.writeFileSync(`./data/${x}`,JSON.stringify("[]"),error=>console.log(error));
+    fs.writeFileSync(`./data/${x}`,JSON.stringify([]),error=>console.log(error));
 });
